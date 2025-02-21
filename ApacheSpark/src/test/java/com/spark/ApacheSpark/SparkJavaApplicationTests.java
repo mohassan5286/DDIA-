e0ee44780408bdf369b8c28a63d81882ab1cd13f
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2025-02-21 18:50:50 +0200

    pom is ok

---
+++ ApacheSpark/src/test/java/com/spark/ApacheSpark/SparkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.ApacheSpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
